a3609b20ec9a2fd5ce322721d6a53d6814fdd086
Igor Popach <igorpopach@step.itstep.rv.ua>
2018-06-25 20:56:56 +0300

    lesson1

--- js/main.js
+++ js/main.js
@@ -1,42 +1,96 @@
-var a = 10;
-console.log(a);
-console.log(typeof(a));
-var b = 3.14;
-console.log(b);
-console.log(typeof(b));
+// var a = 10;
+// console.log(a);
+// console.log(typeof(a));
+// var b = 3.14;
+// console.log(b);
+// console.log(typeof(b));
+//
+// var STR = "Hello world!";
+// console.log(STR);
+// console.log(typeof(STR));
+//
+// var c = true;
+// console.log(c);
+// console.log(typeof(c));
+// c = false;
+// console.log(c);
+// console.log(typeof(c));
+//
+// var d = null;
+// console.log(d);
+// console.log(typeof(d));
+//
+// var e;
+// console.log(e);
+// console.log(typeof(e));
+//
+// var person = {
+//     name:"Tom"
+//
+// }
+// console.log(person);
+// console.log(typeof(person));
+//
+// var arr = [3,8,"test",'bill',true];
+// console.log(arr);
+// console.log(typeof(arr));
+//
+// console.log(typeof(function(){}));
+//
+// var global = this;
+// console.log(global);
+// console.log(typeof(global));
+//
+//
+// var a = 10;
+// var b = 20;
+// var res = a + b;
+// console.log(res);
+//
+// var str1 = "Hello!";
+// res = a + str1;
+// console.log(res);
+// console.log(typeof(res));
 
-var STR = "Hello world!";
-console.log(STR);
-console.log(typeof(STR));
-
-var c = true;
-console.log(c);
-console.log(typeof(c));
-c = false;
-console.log(c);
-console.log(typeof(c));
-
-var d = null;
-console.log(d);
-console.log(typeof(d));
-
-var e;
-console.log(e);
-console.log(typeof(e));
-
-var person = {
-    name:"Tom"
-
+var a = 110, b = 220;
+if ( a > b ) {
+    console.log("a: " + a + " b: " + b);
 }
-console.log(person);
-console.log(typeof(person));
+else if ( a < b ) {
+    console.log("a < b");
+}
+else {
+    console.log("a = b");
+}
 
-var arr = [3,8,"test",'bill',true];
-console.log(arr);
-console.log(typeof(arr));
 
-console.log(typeof(function(){}));
+var a = "5";
+var b = 5;
+if ( a === b ) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
 
-var global = this;
-console.log(global);
-console.log(typeof(global));
+var a = 5;
+var b = 10;
+var sign = "-";
+// if (sign === "-") {
+//     console.log(a - b);
+// } else if (sign === "+") {
+//     console.log(a + b);
+// } else if (sign === "*") {
+//     console.log(a * b);
+// } else {
+//     console.log(a / b);
+// }
+
+switch ( sign ) {
+    case "+" : console.log(a + b);
+    break;
+    case "-" : console.log(a - b);
+    break;
+    default : console.log("R.T.F.M.");
+    break;
+}
