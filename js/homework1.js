f96ade32da6aaceffe161b629272deb144966c92
unknown <igorpopach@gmail.com>
2018-07-04 15:50:20 +0300

    homework 04/07/18

--- js/homework1.js
+++ js/homework1.js
@@ -15,13 +15,104 @@ function simbols(){
 }
 
 function sumNum(){
-    var number = prompt('How many numbers do you want to show?','1');
+    var a = prompt('Вкажіть початок діапазону чисел','0');
+    var b = prompt('Вкажіть кінець діапазону чисел','0');
     var result = 0;
-    for (var i = 1; i <= number; i++) {
-        var num = +prompt('Enter number, please','0');
-        if ( num % 2 != 0 && (num ^ 0) === num) {
-            result += num;
+    for (var i = a; i <= b; i++) {
+        if ( i % 2 != 0 && (i ^ 0) === i) {
+            result += i;
         }
     }
     document.write('Sum of your numbers is: ' + result);
+}
+
+function burgerShop() {
+    var size = prompt("Доброго дня!!! Бажаєте бургер?\n" +
+        "Введіть будь-ласка розмір:\n" +
+        "Big (ціна 25грн, 340 ккал) чи Small (ціна 17грн, 250 ккал)","");
+    var options = prompt("Оберіть будь-ласка числом пункт меню з інгредієнтами:\n" +
+        "1. Сир (+ 4грн, + 25 ккал);\n" +
+        "2. Салат (+ 5грн, + 5 ккал);\n" +
+        "3. Шинка (+ 10грн, + 50 ккал);\n" +
+        "4. Сир + салат (+ 9грн, + 30 ккал);\n" +
+        "5. Салат + шинка (+ 15грн, + 55 ккал);\n" +
+        "6. Сир + шинка (+ 14грн, + 75 ккал);\n" +
+        "7. Сир + салат + шинка (+ 19грн, + 80 ккал);");
+    var add = prompt("Оберіть будь-ласка числом пункт меню з додатковими інгредієнтами:\n" +
+        "1. Соус (+5грн, 0 ккал);\n" +
+        "2. Майонез (+ 4грн, + 10 ккал);\n" +
+        "3. Соус та майонез (+ 9грн, + 10 ккал);");
+    var result = 0;
+    var calories = 0;
+    var resultAdd = 0;
+    var caloriesAdd = 0;
+    var resultAll = 0;
+    var caloriesAll = 0;
+
+    if (size == "Big") {
+        switch (options) {
+            case "1": result = 4; calories = 25;
+                break;
+            case "2": result = 5; calories = 25;
+                break;
+            case "3": result = 10; calories = 50;
+                break;
+            case "4": result = 9; calories = 30;
+                break;
+            case "5": result = 15; calories = 55;
+                break;
+            case "6": result = 14; calories = 75;
+                break;
+            case "7": result = 19; calories = 80;
+                break;
+            default : result = 0; calories = 0;
+        }
+        switch (add) {
+            case "1": resultAdd = 5; caloriesAdd = 0;
+                break;
+            case "2": resultAdd = 4; caloriesAdd = 10;
+                break;
+            case "3": resultAdd = 9; caloriesAdd = 10;
+                break;
+            default : resultAdd = 0; caloriesAdd = 0;
+        }
+        resultAll = 25 + result + resultAdd;
+        caloriesAll = 340 + calories + caloriesAdd;
+        document.write("Повна вартість вашого замовлення становить:  " + resultAll + " грн" + "<br>"
+            + "Ого! Калорійність вашого бургера:  " + caloriesAll + "ккал");
+    } else if (size == "Small") {
+        switch (options) {
+            case "1": result = 4; calories = 25;
+                break;
+            case "2": result = 5; calories = 25;
+                break;
+            case "3": result = 10; calories = 50;
+                break;
+            case "4": result = 9; calories = 30;
+                break;
+            case "5": result = 15; calories = 55;
+                break;
+            case "6": result = 14; calories = 75;
+                break;
+            case "7": result = 19; calories = 80;
+                break;
+            default : result = 0; calories = 0;
+        }
+        switch (add) {
+            case "1": resultAdd = 5; caloriesAdd = 0;
+                break;
+            case "2": resultAdd = 4; caloriesAdd = 10;
+                break;
+            case "3": resultAdd = 9; caloriesAdd = 10;
+                break;
+            default : resultAdd = 0; caloriesAdd = 0;
+        }
+        resultAll = 17 + result + resultAdd;
+        caloriesAll = 250 + calories + caloriesAdd;
+        document.write("Повна вартість вашого замовлення становить:  " + resultAll + " грн" + "<br>"
+            + "Ого! Калорійність вашого бургера:  " + caloriesAll + "ккал");
+    } else {
+        document.write("Упс! Ви не ввели необхідні дані! Повторіть будь-ласка ще раз :)")
+    }
+
 }
