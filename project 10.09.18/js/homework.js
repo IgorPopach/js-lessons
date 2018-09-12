2365d9cbf8905bbf8bc14bec994d378e27a56d43
Igor Popach <igorpopach@gmail.com>
2018-09-12 16:46:47 +0300

    homework 12.09.18 (Ombd api Not work!)

---
+++ project 10.09.18/js/homework.js
@@ -0,0 +1,41 @@
+function httpGet(url) {
+
+    return new Promise(function(resolve, reject) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+
+        xhr.onload = function() {
+            if (this.status == 200) {
+                resolve(this.response);
+            } else {
+                var error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        };
+
+        xhr.onerror = function() {
+            reject(new Error("Network Error"));
+        };
+
+        xhr.send();
+    });
+
+}
+
+
+httpGet("http://www.omdbapi.com/?t=terminator&apikey=6c00430a")
+    .then(
+        response => {
+            console.log(response);
+            let img = new Image();
+            img.src = "http://img.omdbapi.com/?t=terminator&apikey=6c00430a";
+            console.log(img.src);
+            document.body.appendChild(img);
+        },
+        error => alert(`Rejected: ${error}`)
+    );
+
+
+// "http://img.omdbapi.com/?t=terminator&apikey=6c00430a"
