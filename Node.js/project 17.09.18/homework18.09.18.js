c2904592c6d481f2b6b128290f3182402d40a70e
unknown <igorpopach@gmail.com>
2018-09-19 12:10:14 +0300

    update homework19.09.18

--- Node.js/project 17.09.18/homework18.09.18.js
+++ Node.js/project 17.09.18/homework18.09.18.js
@@ -17,18 +17,15 @@ stdin.on("data", data => {
     if(answers.length < questions.length) {
         ask(answers.length)
     } else {
-        console.log(answers);
-        process.exit;  
+        fs.writeFile("person1.json", `{\n "name": "${answers[0]}",\n "surname": "${answers[1]}",\n "age": "${answers[2]}"\n}`, err => {
+            if(err) throw (err);
+            console.log("\nOmg!!! O_o\nYour data save!!!\n");
+            process.exit();
+        })
     }
 });
-
 process.on('exit', function() {
-    fs.writeFile("person1.json", `{\n "name": "${answers[0]}",\n "surname": "${answers[1]}",\n "age": "${answers[2]}"\n}`, err => {
-    if(err) throw (err);
-    console.log("data save");
-    });
-    console.log(answers);
-    let obj = require("./showPerson.js");
+    stdout.write(`Now you can see your info!!! Just input - node showPerson\n`);
 });
 
 ask(0);
--- Node.js/project 17.09.18/showPerson.js
+++ Node.js/project 17.09.18/showPerson.js
@@ -2,5 +2,6 @@ const fs = require('fs');
 const dir = fs.readdirSync(__dirname);
 fs.readFile("person1.json", (err, dir) => {
     if (err) throw (err);
-    console.log(JSON.parse(dir.toString()).name);
+    let obj = JSON.parse(dir.toString());
+    console.log(`\n\nHello ${obj.name} ${obj.surname}!!!\nYour age is ${obj.age}. Ooops...you are so old!:(\n\n`);
 })
