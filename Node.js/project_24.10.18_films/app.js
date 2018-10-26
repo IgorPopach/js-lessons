5e46e03ffe4ac1b33c4f3a1420f706d5fb0b090b
unknown <igorpopach@gmail.com>
2018-10-26 12:09:21 +0300

    fixed route search

--- Node.js/project_24.10.18_films/app.js
+++ Node.js/project_24.10.18_films/app.js
@@ -3,21 +3,22 @@ const content = require('./routes/content');
 const home = require('./routes/home');
 const search = require('./routes/search');
 const notFound = require('./routes/notFound');
+const url = require('url');
 
 http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
     if (req.url.match(/\.(html|css|js|png|eot|ico|svg|ttf|woff|woff2)$/)) {
         content(req, res);
     }
     else if (req.url === '/') {
         home(req, res);
     }
-    else if (req.url === '/search') {
+    else if (parsedUrl.pathname === '/search') {
         search(req,res);
     }
     else {
         notFound(req,res);
     }
-
-    // console.log(req.url);
 
 }).listen('80', () => console.log('running...'));
