ce41d619c22c0e23425e61c8841f55fc3ecbbda2
unknown <igorpopach@gmail.com>
2018-10-26 11:46:14 +0300

    update search route

--- Node.js/project_24.10.18_films/routes/search.js
+++ Node.js/project_24.10.18_films/routes/search.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const request = require('../modules/request');
+const url = require('url');
 
 function search(req,res) {
     const parsedUrl = url.parse(req.url, true);
     console.log(`req ${req}`);
+    console.log(`parsedUrl ${parsedUrl}`);
     const title = parsedUrl.query.title;
     console.log(title);
     
