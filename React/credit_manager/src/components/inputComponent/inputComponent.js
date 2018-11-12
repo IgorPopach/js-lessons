a4d95292707f19306a7c5f17b619b3ad0261622d
unknown <igorpopach@gmail.com>
2018-11-12 12:50:26 +0200

    modified inputComponent

--- React/credit_manager/src/components/inputComponent/inputComponent.js
+++ React/credit_manager/src/components/inputComponent/inputComponent.js
@@ -10,32 +10,48 @@ class InputComponent extends Component {
             amount: "",
             term: "",
         };
+        this.submitHendler=this.submitHendler.bind(this);
+        this.handleNameChange=this.handleNameChange.bind(this);
+        this.handleSurnameChange=this.handleSurnameChange.bind(this);
+        this.handleAmountChange=this.handleAmountChange.bind(this);
+        this.handleTermChange=this.handleTermChange.bind(this);
     }
+
+    handleNameChange = (event) => {
+        this.setState({name: event.target.value})
+    }
+
+    handleSurnameChange = (event) => {
+        this.setState({surname: event.target.value})
+    }
+    handleAmountChange = (event) => {
+        this.setState({amount: event.target.value})
+    }
+    handleTermChange = (event) => {
+        this.setState({term: event.target.value})
+    }
+
     submitHendler = (event) => {
         event.preventDefault();
-        console.log(this.refs.clientName.value + "\n" + this.refs.clientSurname.value + "\n" + this.refs.amount.value + "\n" + this.refs.term.value);
-        this.setState({
-            name:this.refs.clientName.value,
-            surname:this.refs.clientSurname.value,
-            amount:this.refs.amount.value,
-            term:this.refs.term.value,
-        })
-        console.log(" state: " + this.state.name);
+        console.log(this.state);
         let count = 0;
         const usersRef = firebase.database().ref("users/");
         usersRef.on("child_added", function(data) {
             let userBase = data.val();
-            if ( userBase.username === this.refs.clientName.value && userBase.surname === this.refs.clientSurname.value) {
+            //fix me start!!!!
+            console.log("this.state.name: " + this.state.name);
+            if ( userBase.username === this.state.name && userBase.surname === this.state.surname) {
                 count++;
             }
+            //fix me end!!!!
         });
         if (count <= 2) {
             const newClientKey = firebase.database().ref().child('users').push().key;
             firebase.database().ref('users/' + newClientKey).set({
-                username: this.refs.clientName.value,
-                surname: this.refs.clientSurname.value,
-                amount : this.refs.amount.value,
-                term : this.refs.term.value
+                username: this.state.name,
+                surname: this.state.surname,
+                amount : this.state.amount,
+                term : this.state.term
             });
             console.log("Look to your Base!");
         } else {
@@ -45,30 +61,30 @@ class InputComponent extends Component {
     render() {
         return (
             <div>
-                <form className="col-md-6 form-line">
+                <form className="col-md-6 form-line" onSubmit={this.submitHendler}>
                     <div className="form-group">
                         <label>Client name</label>
-                        <input type="text" className="form-control" name="clientName" placeholder=" Enter Name" required ref={"clientName"}></input>
+                        <input type="text" className="form-control" name="clientName" placeholder=" Enter Name" value={this.state.name} onChange={this.handleNameChange}></input>
                     </div>
                     <div className="form-group">
                         <label>Client surname</label>
                         <input type="text" className="form-control" name="clientSurname"
-                            placeholder=" Enter client surname" required ref={"clientSurname"}></input>
+                            placeholder=" Enter client surname" value={this.state.surname} onChange={this.handleSurnameChange}></input>
                     </div>
                     <div className="form-group">
                         <label>Amount</label>
-                        <input type="number" className="form-control" name="amount" placeholder=" Enter amount" required ref={"amount"}></input>
+                        <input type="number" className="form-control" name="amount" placeholder=" Enter amount" value={this.state.amount} onChange={this.handleAmountChange}></input>
                     </div>
                     <div className="form-group">
                         <label>Term</label>
                         <input type="number" className="form-control" maxLength="2" min="3" max="12" name="term"
-                            placeholder=" Enter term" required ref={"term"}></input>
+                            placeholder=" Enter term" value={this.state.term} onChange={this.handleTermChange}></input>
                     </div>
                     <div>
-                        <button type="button" className="btn btn-default submit" name="submit" onClick={this.submitHendler}><i className="fa fa-paper-plane"
+                        <button type="submit" className="btn btn-default submit" name="submit"><i className="fa fa-paper-plane"
                             aria-hidden="true"></i>
                             Send
-                    </button>
+                        </button>
                     </div>
                 </form>
             </div>
