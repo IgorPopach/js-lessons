a23c79ff81787da41ea5a74c00ee00a81e7d728a
unknown <igorpopach@gmail.com>
2018-12-05 01:39:11 +0200

    add localStorage

--- React/lesson_10/src/components/blogList/blogList.js
+++ React/lesson_10/src/components/blogList/blogList.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import BlogItem from './../blogItem/blogItem';
 
+const default_posts = [
+    {
+        id:1,
+        author: 'Igor Popach',
+        date: '03.12.2018',
+        title: 'Music bla bla',
+        desc: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.',
+    },
+    {
+        id:2,
+        author: 'Sara Conor',
+        date: '03.12.2018',
+        title: 'Why do we use it?',
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+        id:3,
+        author: 'Elvis Presley',
+        date: '03.12.2018',
+        title: 'Where does it come from?',
+        desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, comes from a line in section 1.10.32.",
+    },
+]
 
 class BlogList extends React.Component{
     state = {
-        posts: [
-            {
-                id:1,
-                author: 'Igor Popach',
-                date: '03.12.2018',
-                title: 'Music bla bla',
-                desc: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.',
-            },
-            {
-                id:2,
-                author: 'Sara Conor',
-                date: '03.12.2018',
-                title: 'Why do we use it?',
-                desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            },
-            {
-                id:3,
-                author: 'Elvis Presley',
-                date: '03.12.2018',
-                title: 'Where does it come from?',
-                desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, comes from a line in section 1.10.32.",
-            },
-        ]
+        
     }
+
+    getPosts = () => {
+        const posts = JSON.parse(localStorage.getItem('posts'));
+        this.setState({ posts, })
+    }
+    pushToStorDefaultPosts(){
+        localStorage.setItem('posts', JSON.stringify(default_posts));
+        this.setState({ posts: default_posts, }) // TODO setState underfined
+    }
+
+
     postHandler = (path,properties) => {
         console.log('path',path)
         this.props.history.push({
@@ -40,13 +52,25 @@ class BlogList extends React.Component{
             pathname:'/',
         })
     }
+    componentWillUpdate(){
+        console.log('componentWillUpdate')
+    }
+    componentDidMount(){
+        this.getPosts()
+    }
+
     render(){
         console.log('BlogList', this.props)
         return(
             <div className='row justify-content-center bcg-style'>
-                {this.state.posts.map((e) => (
-                    <BlogItem key={e.id} id={e.id} author={e.author} title={e.title} date={e.date} desc={e.desc} postHandler={this.postHandler} pathName={this.props.match.path} />
-                    ))
+                { this.state.posts ? 
+                    this.state.posts.map((e) => (
+                        <BlogItem key={e.id} id={e.id} author={e.author} title={e.title} date={e.date} desc={e.desc} postHandler={this.postHandler} pathName={this.props.match.path} />
+                    )) :
+                    <div className='col-12'>
+                        <p>Click "Download" posts from storage"</p>
+                        <button onClick={this.pushToStorDefaultPosts} className='btn btn-secondary'>Download</button>
+                    </div>
                 }
                 <button onClick={this.homeHandler} className='btn btn-secondary'>Home</button>
             </div>
